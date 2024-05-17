09b0df5994dfa44727650e38688c0daf54d21c0a
Kritik Patidar <102941982+PatidarManish@users.noreply.github.com>
2024-05-17 14:16:08 +0530

    Added project configuration

---
+++ Fishery/Fishery/FisheryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FisheryApp.swift
+//  Fishery
+//
+//  Created by Manish Patidar on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FisheryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
